cc6a633829ec85310ac39cd4c37ae9a6d692e444
cookie-god <cookie-god@softsquared.com>
2021-10-25 20:16:40 +0900

    ⚡️ swagger 적용 완료

--- swagger/userSwagger.js
+++ swagger/userSwagger.js
@@ -1,6 +1,118 @@
 /**
  * @swagger
- * /users/signup:
+ *
+ * /app/user-check:
+ *  post:
+ *      tags:
+ *      - users
+ *      description: 회원 가입전 검증 API
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: body
+ *          name: body
+ *          required: true
+ *          schema:
+ *              type: object
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                      required: true
+ *                      description: 이름
+ *                      example: 구기성
+ *                  nickname:
+ *                      type: string
+ *                      required: true
+ *                      description: 닉네임
+ *                      example: 쿠키
+ *                  gender:
+ *                      type: integer
+ *                      required: true
+ *                      description: 성별(1:남자,2:여자)
+ *                      example: 1
+ *                  phoneNumber:
+ *                      type: string
+ *                      required: true
+ *                      description: 핸드폰 번호
+ *                      example: 01012341234
+ *                  birthday:
+ *                      type: string
+ *                      required: true
+ *                      description: 생년월일
+ *                      example: 19960906
+ *                  email:
+ *                      type: string
+ *                      required: true
+ *                      description: 이메일
+ *                      example: cookie-god@softsquared.com
+ *                  password:
+ *                      type: string
+ *                      required: true
+ *                      description: 비밀번호
+ *                      example: cookie7869
+ *                  confrimPassword:
+ *                      type: string
+ *                      required: true
+ *                      description: 확인 비밀번호
+ *                      example: cookie7869
+ *
+ *      responses:
+ *        1000:
+ *          description: 성공
+ *
+ *        2001:
+ *          description: 이름을 입력해주세요.
+ *
+ *        2002:
+ *          description: 닉네임을 입력해주세요.
+ *
+ *        2003:
+ *          description: 닉네임은 2~20자리로 입력해주세요.
+ *
+ *        2004:
+ *          description: 성별을 선택해주세요.
+ *
+ *        2005:
+ *          description: 성별형식을 정확하게 입력해주세요.
+ *
+ *        2006:
+ *          description: 핸드폰 번호를 선택해주세요.
+ *
+ *        2007:
+ *          description: 핸드폰 번호을 정확하게 입력해주세요.
+ *
+ *        2008:
+ *          description: 생년월일을 입력해주세요.
+ *
+ *        2009:
+ *          description: 생년월일 형식을 정확하게 입력해주세요.
+ *
+ *        2010:
+ *          description: 이메일을 입력해주세요.
+ *
+ *        2011:
+ *          description: 이메일을 형식을 정확하게 입력해주세요.
+ *
+ *        2012:
+ *          description: 비밀번호를 입력 해주세요.
+ *
+ *        2013:
+ *          description: 비밀번호 형식을 정확하게 입력해주세요.
+ *
+ *        2014:
+ *          description: 확인 비밀번호를 입력 해주세요.
+ *
+ *        2015:
+ *          description: 비밀번호와 확인 비밀번호가 일치하지 않습니다.
+ *
+ *        2016:
+ *          description: 이미 존재하는 핸드폰 번호입니다.
+ *
+ *        2017:
+ *          description: 이미 존재하는 이메일입니다.
+ *
+ *
+ * /app/users:
  *  post:
  *      tags:
  *      - users
@@ -14,103 +126,143 @@
  *          schema:
  *              type: object
  *              properties:
- *                  id:
+ *                  name:
  *                      type: string
  *                      required: true
- *                      description: 사용자 아이디
- *                      example: kookisung
- *                  pw:
- *                      type: string
- *                      required: true
- *                      description: 사용자 비밀번호
- *                      example: rnrltjd7869!
- *                  email:
- *                      type: string
- *                      required: true
- *                      description: 사용자 이메일
- *                      example: lion0193@gmail.com
+ *                      description: 이름
+ *                      example: 구기성
  *                  nickname:
  *                      type: string
  *                      required: true
- *                      description: 사용자 닉네임
- *                      example: 구기성
+ *                      description: 닉네임
+ *                      example: 쿠키
  *                  gender:
+ *                      type: integer
+ *                      required: true
+ *                      description: 성별(1:남자,2:여자)
+ *                      example: 1
+ *                  phoneNumber:
  *                      type: string
  *                      required: true
- *                      description: 사용자 성별
- *                      example: M
+ *                      description: 핸드폰 번호
+ *                      example: 01012341234
  *                  birthday:
  *                      type: string
  *                      required: true
- *                      description: 사용자 생년월일
- *                      example: 1996-09-06
- *                  isIos:
+ *                      description: 생년월일
+ *                      example: 19960906
+ *                  email:
  *                      type: string
- *                      required: false
- *                      description: 모바일 운영체제
- *                      example: N
+ *                      required: true
+ *                      description: 이메일
+ *                      example: cookie-god@softsquared.com
+ *                  password:
+ *                      type: string
+ *                      required: true
+ *                      description: 비밀번호
+ *                      example: cookie7869
+ *                  isPermitAlarm:
+ *                      type: string
+ *                      required: true
+ *                      description: 알람 수신 동의 여부(1:수신,2:수신안함)
+ *                      example: 1
  *
  *      responses:
- *        200:
- *          description: 회원가입 성공
+ *        1000:
+ *          description: 성공
  *
- *        301:
- *          description: 아이디를 입력해주세요.
+ *        2001:
+ *          description: 이름을 입력해주세요.
  *
- *        302:
- *          description: 아이디는 6~15자리로 입력해주세요.
- *
- *        303:
- *          description: 아이디는 영어 대소문자, 숫자로만 구성됩니다.
- *
- *        304:
- *          description: 비밀번호를 입력해주세요.
- *
- *        305:
- *          description: 비밀번호는 10~16자리로 입력해주세요.
- *
- *        306:
- *          description: 비밀번호는 영문, 숫자, 특수문자로 이뤄져야 합니다.
- *
- *        307:
- *          description: 이메일을 입력해주세요.
- *
- *        308:
- *          description: 유효하지 않은 이메일입니다.
- *
- *        309:
+ *        2002:
  *          description: 닉네임을 입력해주세요.
  *
- *        310:
- *          description: 닉네임은 특수문자를 제외한 1-10자로 입력해주세요.
+ *        2003:
+ *          description: 닉네임은 2~20자리로 입력해주세요.
  *
- *        311:
- *          description: 닉네임은 한글로만 입력해주세요.
+ *        2004:
+ *          description: 성별을 선택해주세요.
  *
- *        312:
- *          description: 성별을 입력해주세요.
+ *        2005:
+ *          description: 성별형식을 정확하게 입력해주세요.
  *
- *        313:
- *          description: 성별은 M 또는 F 입니다.
+ *        2006:
+ *          description: 핸드폰 번호를 선택해주세요.
  *
- *        314:
- *          description: 생년월일이 비어있습니다.
+ *        2007:
+ *          description: 핸드폰 번호을 정확하게 입력해주세요.
  *
- *        315:
- *          description: 생년월일이 yyyy-mm-dd 형식이 아닙니다.
+ *        2008:
+ *          description: 생년월일을 입력해주세요.
  *
- *        316:
- *          description: 모바일 운영체제 선택이 잘못됐습니다.
+ *        2009:
+ *          description: 생년월일 형식을 정확하게 입력해주세요.
  *
- *        317:
- *          description: 중복된 아이디입니다.
+ *        2010:
+ *          description: 이메일을 입력해주세요.
  *
- *        318:
- *          description: 중복된 닉네임입니다.
+ *        2011:
+ *          description: 이메일을 형식을 정확하게 입력해주세요.
  *
- *        319:
- *          description: 중복된 이메일입니다.
+ *        2012:
+ *          description: 비밀번호를 입력 해주세요.
  *
- *        500:
- *          description: sql문 에러입니다.
+ *        2013:
+ *          description: 비밀번호 형식을 정확하게 입력해주세요.
+ *
+ *        2016:
+ *          description: 이미 존재하는 핸드폰 번호입니다.
+ *
+ *        2017:
+ *          description: 이미 존재하는 이메일입니다.
+ *
+ *        2018:
+ *          description: 푸시알람 입력 형식을 정확하게 입력해주세요.
+ *
+ * /app/login:
+ *  post:
+ *      tags:
+ *      - users
+ *      description: 로그인 API
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: body
+ *          name: body
+ *          required: true
+ *          schema:
+ *              type: object
+ *              properties:
+ *                  email:
+ *                      type: string
+ *                      required: true
+ *                      description: 이메일
+ *                      example: cookie-god@softsquared.com
+ *                  password:
+ *                      type: string
+ *                      required: true
+ *                      description: 비밀번호
+ *                      example: cookie7869
+ *
+ *      responses:
+ *        1000:
+ *          description: 성공
+ *
+ *        2010:
+ *          description: 이메일을 입력해주세요.
+ *
+ *        2011:
+ *          description: 이메일을 형식을 정확하게 입력해주세요.
+ *
+ *        2012:
+ *          description: 비밀번호를 입력 해주세요.
+ *
+ *        2013:
+ *          description: 비밀번호 형식을 정확하게 입력해주세요.
+ *
+ *        2023:
+ *          description: 존재하지 않는 이메일입니다.
+ *
+ *        2024:
+ *          description: 비밀번호가 일치하지 않습니다.
  */
