c87397c4e8892498b0352d973f9c29174ed0b31c
cookie-god <cookie-god@softsquared.com>
2021-12-09 19:20:00 +0900

    🐛 비밀번호 정규식 변경

--- config/regularExpress.js
+++ config/regularExpress.js
@@ -2,5 +2,5 @@ module.exports = {
     birthdayRegex : /^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/,
     emailRegex : /^([\w_\.\-\+])+\@([\w\-]+\.)+([\w]{2,10})+$/,
     passwordRegex : /^[a-zA-Z0-9]{8,20}$/,
-    phoneNumberRegex : /^\d{2,3}-\d{3,4}-\d{4}$/
+    phoneNumberRegex : /^(01\d{1})-([0-9]{3,4})-([0-9]{4})$/
 };
