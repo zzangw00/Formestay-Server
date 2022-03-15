34e50223c7fe77c41210dc5e344b9645f18b4bd5
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-03-15 16:54:27 +0900

    Modify: .gitignore

--- config/secret.js
+++
@@ -1,7 +0,0 @@
-// TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
-// TODO: .gitignore에 추가하는거 앚지 마세요!
-module.exports = {
-    'jwtsecret' :  'softsquared_jwt_secret_key_07040014087',
-    'bootpay_application_id' : '619bd13be38c300022d2bc04',
-    'bootpay_private_key' : 'HI5T8vn2x/fN4NnDdqZIKRZ+VX/D2HgHGZfoku+Km98='
-};
